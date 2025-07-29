f89c8d87df5a7d2ed53c2c4c9945020c1bf4860e
leonfiasco <leon_adjei@hotmail.com>
2025-07-29 19:56:10 +0100

    adding logs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,24 +13,48 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>
 ) -> HttpResponse {
-    let query_result = sqlx::query!(
+let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
+    match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
-        Uuid::new_v4(),
+        request_id,
         form.email,
         form.name,
         Utc::now()
     )
     .execute(pool.get_ref())
-    .await;
-
-    match query_result {
-        Ok(_) => HttpResponse::Ok().finish(),
+    .await
+    {
+        Ok(_) => {
+            log::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
 }
+
+
+
+
