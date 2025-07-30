d8c7527683f3255948abd8ec83b27ef920005654
leonfiasco <leon_adjei@hotmail.com>
2025-07-30 19:31:02 +0100

    added a tracer logging

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,8 @@ use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-     // `init` does call `set_logger`, so this is all we need to do.
-    // We are falling back to printing all logs at info-level or above 
-    // if the RUST_LOG environment variable has not been set.
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     // Renamed!
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use sqlx::PgPool;  // Changed from PgConnection to PgPool
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -13,48 +14,46 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>
 ) -> HttpResponse {
-let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+ let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
-    log::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id
+    let _request_span_guard = request_span.enter();
+
+    // We do not call `.enter` on query_span!
+    // `.instrument` takes care of it at the right moments
+    // in the query future lifetime
+    let query_span = tracing::info_span!(
+        "Saving new subscriber details in the database"
     );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
-        request_id,
+        Uuid::new_v4(),
         form.email,
         form.name,
         Utc::now()
     )
     .execute(pool.get_ref())
+    // First we attach the instrumentation, then we `.await` it
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
-            log::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
             HttpResponse::Ok().finish()
         },
         Err(e) => {
-            log::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            // Yes, this error log falls outside of `query_span`
+            // We'll rectify it later, pinky swear!
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
 }
-
-
 
 
